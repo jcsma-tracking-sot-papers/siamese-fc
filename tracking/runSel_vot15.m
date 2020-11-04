7d11b7387979b8b46f17d8e52cada22b66e9ea8e
Juan C. SanMiguel <juancarlos.sanmiguel@uam.es>
2020-11-04 09:33:30 +0100

    script to run selected sequences

--- tracking/runSel_vot15.m
+++ tracking/runSel_vot15.m
@@ -6,9 +6,9 @@ function runSel_vot15
     visualization = true;
     gpus = 0;
 
-    seq={
-    'vot15_bag'    
-    };
+    seq={'vot15_bag'};
+    seq={'vot15_basketball'};
+    seq={'vot15_motocross1'};
 
     for s=1:numel(seq)
        run_tracker(seq{s}, visualization);
